dafd9f9fc5f18769d5a2b059a9811dbdb9d4519e
kiran-1906 <kiran1906@vt.edu>
2023-11-03 11:51:26 -0400

    styled recruiterlanding page

--- src/frontend/components/recruiterLanding.js
+++ src/frontend/components/recruiterLanding.js
@@ -1,50 +1,88 @@
 import React, { useState, useEffect } from "react";
+import "../css/recruiterLanding.css";
 
 const DisplayJobSeekers = () => {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-        // Fetch data when the component mounts
-        fetchData();
-    }, []);
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch('/api/jobSeekers', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
+  const handleNext = () => {
+    if (currentIndex < users.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
+  };
 
-            if (response.ok) {
-                const userData = await response.json();
-                setUsers(userData); // Update the state with the fetched data
-            } else {
-                const errorMessage = await response.text();
-                alert(`Error: ${errorMessage}`);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+  };
 
-    return (
-        <div>
-            <h1>User List</h1>
-            <ul>
-                {users.map((user, index) => (
-                    <li key={index}>
-                        {/* Render user data here */}
-                        <p>Name: {user.firstName}</p>
-                        <p>Email: {user.email}</p>
-                        <p>phone: {user.phone}</p>
-                        {/* Add more details as needed */}
-                    </li>
-                ))}
-            </ul>
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/jobSeekers", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const userData = await response.json();
+        setUsers(userData); // Update the state with the fetched data
+      } else {
+        const errorMessage = await response.text();
+        alert(`Error: ${errorMessage}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  //   return (
+  //     <div class="recruiter-container">
+  //       <h1>User List</h1>
+  //       <ul>
+  //         {users.map((user, index) => (
+  //           <li key={index}>
+  //             {/* Render user data here */}
+  //             <p>Name: {user.firstName}</p>
+  //             <p>Email: {user.email}</p>
+  //             <p>phone: {user.phone}</p>
+  //             {/* Add more details as needed */}
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //   );
+
+  return (
+    <div className="recruiter-container">
+      <h1>User Details</h1>
+      {users.length > 0 && (
+        <div className="user-card">
+          <p>Name: {users[currentIndex].firstName}</p>
+          <p>Email: {users[currentIndex].email}</p>
+          <p>Phone: {users[currentIndex].phone}</p>
+          <div className="card-navigation">
+            <button onClick={handlePrev} disabled={currentIndex === 0}>
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              disabled={currentIndex === users.length - 1}
+            >
+              Next
+            </button>
+          </div>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default DisplayJobSeekers;
