2bfad37d775930c351582a09746bb719f7c1c5dc
Pranav Vishwanatham <pranavvishwanatham@gmail.com>
2023-11-06 00:20:24 -0500

    Job seeker view page changes

--- src/frontend/components/SponsorsList.js
+++ src/frontend/components/SponsorsList.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/SponsorsList.css';
 
 function SponsorsList(props) {
+    // State to keep track of the selected sponsor
+    const [selectedSponsor, setSelectedSponsor] = useState(null);
+
+    const handleSelect = (sponsor) => {
+        setSelectedSponsor(sponsor);
+        props.onSelect(sponsor);
+    };
+
     return (
         <div className="sponsorsList">
             {props.sponsors.map((sponsor, index) => (
-                <div key={index} onClick={() => props.onSelect(sponsor)}>
-                    {sponsor.role}<br></br><br></br>
-                    {sponsor.name}
+                <div
+                    key={index}
+                    onClick={() => handleSelect(sponsor)}
+                    // Add a class to indicate if this sponsor is the selected one
+                    className={selectedSponsor === sponsor ? 'selected' : ''}
+                >
+                    Job Role: {sponsor.role}<br /><br />
+                    Company Name: {sponsor.name}
                 </div>
             ))}
         </div>
