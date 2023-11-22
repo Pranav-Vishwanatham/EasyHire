57aab82458c9f7d2d3a06920147baaa61b2acc63
kiran-1906 <kiran1906@vt.edu>
2023-11-21 21:15:34 -0500

    small changes

--- src/frontend/components/login.js
+++ src/frontend/components/login.js
@@ -1,74 +1,86 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../css/login.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/login.css";
 
 function Login() {
-    const navigate = useNavigate(); // Get the history object from React Router
+  const navigate = useNavigate(); // Get the history object from React Router
 
-    const handleSuccessfulLogin = (user) => {
-        console.log(`Hello ${user.firstName}! You are successfully logged in!`);
-        if(user.designation == 'jobSeeker') {
-            console.log("entered successful login");
-            navigate('/companies'); 
-        }
-        else if(user.designation == 'recruiter') {
-            navigate('/jobSeekers'); 
-        }
-        // Perform the routing to the 'jobSeekers' component after successful login
-    };
+  const handleSuccessfulLogin = (user) => {
+    console.log(`Hello ${user.firstName}! You are successfully logged in!`);
+    if (user.designation === "jobSeeker") {
+      console.log("entered successful login");
+      navigate("/companies");
+    } else if (user.designation === "recruiter") {
+      navigate("/jobSeekers");
+    }
+    // Perform the routing to the 'jobSeekers' component after successful login
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const loginEmail = document.getElementById('email-txt').value;
-        const loginPassword = document.getElementById('password').value;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const loginEmail = document.getElementById("email-txt").value;
+    const loginPassword = document.getElementById("password").value;
 
-        // Do something with the email and password, for example, log them to the console
-        console.log('Email:', loginEmail);
-        console.log('Password:', loginPassword);
+    // Do something with the email and password, for example, log them to the console
+    console.log("Email:", loginEmail);
+    console.log("Password:", loginPassword);
 
-        try {
-            const response = await fetch('/validate', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ loginEmail, loginPassword })
-            });
+    try {
+      const response = await fetch("/validate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ loginEmail, loginPassword }),
+      });
 
-            if (response.ok) {
-                const userData = await response.json();
-                console.log(userData);
-                alert(`Hello ${userData.firstName}`);
-                handleSuccessfulLogin(userData);
-            } else {
-                const errorMessage = await response.json();
-                alert(`Error: ${errorMessage["error"]}`);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            alert(`Error: ${error}`);
-        }
-    };
+      if (response.ok) {
+        const userData = await response.json();
+        console.log(userData);
+        alert(`Hello ${userData.firstName}`);
+        handleSuccessfulLogin(userData);
+      } else {
+        const errorMessage = await response.json();
+        alert(`Error: ${errorMessage["error"]}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert(`Error: ${error}`);
+    }
+  };
 
-    return (
-        <div>
-            <div className="login-container">
-                <div className="login-card">
-                    <div className="login-header">
-                        <h2>Welcome Back</h2>
-                    </div>
-                    <form className="login-form" onSubmit={handleSubmit}>
-                        <input type="email" id='email-txt' placeholder="Email Address" required />
-                        <input type="password" id='password' placeholder="Password" required />
-                        <br/><br/>
-                        <button type="submit">LOG IN</button>
-                    </form>
-                    <br/>
-                    <a href="#" className="forgot-password">FORGOT PASSWORD?</a>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="login-container">
+        <div className="login-card">
+          <div className="login-header">
+            <h2>Welcome Back</h2>
+          </div>
+          <form className="login-form" onSubmit={handleSubmit}>
+            <input
+              type="email"
+              id="email-txt"
+              placeholder="Email Address"
+              required
+            />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+            <br />
+            <br />
+            <button type="submit">LOG IN</button>
+          </form>
+          <br />
+          <a href="#" className="forgot-password">
+            FORGOT PASSWORD?
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Login;
