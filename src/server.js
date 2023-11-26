b01396178779cb366054653e8956f8afc3f1af9a
GayathriPendyala <gayathripendyla@vt.edu>
2023-11-26 18:35:59 -0500

    added emailservice

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const nodemailer = require('nodemailer');
 require("./backend/db/mongoose");
 const cors = require('cors');
 const loginRoute = require('./backend/routes/loginRoutes');
@@ -7,10 +8,9 @@ const jobSeekersRoute = require('./backend/routes/jobSeekerRoutes');
 const companyRoute = require("./backend/routes/companiesRoutes");
 const recruiterRoute = require( "./backend/routes/recruiterRoutes" );
 const port = 4000; // Define the port number for your server
-
-app.listen(port, () => {
-    console.log(`Server started listening successfully on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server started listening successfully on port ${port}`);
+// });
 
 app.use(express.json());
 app.use(cors());
@@ -26,4 +26,41 @@ app.use(recruiterRoute);
 app.get('/api', (req, res) => {
     // Handle your API routes here
     res.send('API is working');
+});
+
+//------------ email service code ------
+// Email transport configuration
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'kirandot1976@gmail.com',
+        pass: 'pomspuhscwqunbzt'
+    }
+});
+
+// POST endpoint for sending emails
+app.post('/send-email', (req, res) => {
+    const { userEmail, meetingDetails } = req.body;
+
+    const mailOptions = {
+        from: 'kirandot1976@gmail.com',
+        to: userEmail,
+        subject: 'EasyHire Meeting Confirmation',
+        text: `Your meeting is scheduled on ${meetingDetails}.`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error in sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent successfully');
+        }
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
